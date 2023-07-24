a0bf50db443967dc3ebc80d46c238aa5f728ba62
AskNilesh <rathodnilsrk@gmail.com>
2023-07-24 21:58:50 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Nilesh Rathod on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
